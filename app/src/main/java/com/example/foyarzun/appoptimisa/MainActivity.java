181d446a5971449f230d4a470af5b2f23d8a4c0e
Felipe Oyarzun <foyarzun@optimisa.cl>
2018-01-17 10:47:51 -0300

    agregando vista2

--- app/src/main/java/com/example/foyarzun/appoptimisa/MainActivity.java
+++ app/src/main/java/com/example/foyarzun/appoptimisa/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.foyarzun.appoptimisa;
 
+import android.app.Fragment;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button boton =  (Button)findViewById(R.id.GoToVista2);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),Main2Activity.class);
+            startActivityForResult(intent,0);
+
+            }
+
+        });
+
+
+
+
     }
+
+
+
+
 }
