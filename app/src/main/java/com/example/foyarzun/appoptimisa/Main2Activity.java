3edec8495f591122077848878689315585722096
Felipe Oyarzun <foyarzun@optimisa.cl>
2018-01-17 12:42:23 -0300

    borre el cotent 2

--- app/src/main/java/com/example/foyarzun/appoptimisa/Main2Activity.java
+++ app/src/main/java/com/example/foyarzun/appoptimisa/Main2Activity.java
@@ -1,13 +1,9 @@
 package com.example.foyarzun.appoptimisa;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
+
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -15,30 +11,12 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                Button boton =  (Button)findViewById(R.id.INDEX);
-                boton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent(view.getContext(),MainActivity.class);
-                        startActivityForResult(intent,0);
 
-                    }
 
-                });
-            }
-        });
+
 
 
 
     }
-
 }
