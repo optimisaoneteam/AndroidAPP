f11d88eb0a29c8319db511a07078c465cbae2c0a
Ignacio Huichal <ihuichal@optimisa.cl>
2018-01-17 09:44:37 -0300

    Agregando una vista .vista1.

--- app/src/main/java/com/example/foyarzun/appoptimisa/MainActivity.java
+++ app/src/main/java/com/example/foyarzun/appoptimisa/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.foyarzun.appoptimisa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //instancia al boton que esta en main, llamando a su id asociado
+        buttonv1 = (Button)findViewById(R.id.goView1);
+
+        buttonv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //se usa para llamar a otro componentes
+                //se ocupan generalmente para:
+                /*
+                * iniciar actividad
+                * iniciar un servicio
+                * entregar un mensaje
+                * */
+                Intent buttonv1 = new Intent(MainActivity.this,vista1Activity.class);
+                startActivity(buttonv1);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/foyarzun/appoptimisa/vista1Activity.java
@@ -0,0 +1,13 @@
+package com.example.foyarzun.appoptimisa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vista1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista1);
+    }
+}
